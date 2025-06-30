de1d575b0ed3a321faf71d3a26399ac64ca54a53
hyun <ysk30430@naver.com>
2025-06-30 16:04:46 +0900

    feat : text 타입 추가

--- frontend/src/app/shared/ui/text/MediumText.tsx
+++ frontend/src/app/shared/ui/text/MediumText.tsx
@@ -1,6 +1,17 @@
-export function MediumText({ children }: { children: React.ReactNode }) {
+import { TextProps } from './types';
+
+export function MediumText({ 
+  children, 
+  className = '', 
+  onClick,
+  ...props 
+}: TextProps) {
   return (
-    <p className="text-[28px] font-medium text-[var(--color-text-color)] font-pretendard leading-normal">
+    <p 
+      className={`text-[28px] font-medium text-[var(--color-text-color)] font-pretendard leading-normal ${className}`}
+      onClick={onClick}
+      {...props}
+    >
       {children}
     </p>
   );
--- frontend/src/app/shared/ui/text/SemiBoldText.tsx
+++ frontend/src/app/shared/ui/text/SemiBoldText.tsx
@@ -1,6 +1,17 @@
-export function SemiBoldText({ children }: { children: React.ReactNode }) {
+import { TextProps } from './types';
+
+export function SemiBoldText({ 
+  children, 
+  className = '', 
+  onClick,
+  ...props 
+}: TextProps) {
   return (
-    <p className="text-[28px] font-semibold text-[var(--color-text-color)] font-pretendard leading-normal">
+    <p 
+      className={`text-[28px] font-semibold text-[var(--color-text-color)] font-pretendard leading-normal ${className}`}
+      onClick={onClick}
+      {...props}
+    >
       {children}
     </p>
   );
