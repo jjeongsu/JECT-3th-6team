da296f94c140d50ba03c596a8af89e0a9624fa12
hyun <ysk30430@naver.com>
2025-07-02 20:23:32 +0900

    feat: 스타일 수정 - global.css에 등록된 전역변수로 수정 및 pretendard 기본 설정으로 인라인 스타일 삭제

--- frontend/src/app/shared/ui/tag/Tag.tsx
+++ frontend/src/app/shared/ui/tag/Tag.tsx
@@ -4,7 +4,7 @@ import { TagProps } from './types';
 export function Tag({ children, className = '',...props }: TagProps) {
   return (
     <span
-      className={`inline-block mr-1 text-[var(--color-text-gray-color)] font-[var(--font-pretendard)] text-base font-normal leading-[12.584px] ${className}`}
+      className={`inline-block mr-1 text-text-gray-color text-base font-normal leading-[12.584px] ${className}`}
       {...props}
     >
       #{children}
