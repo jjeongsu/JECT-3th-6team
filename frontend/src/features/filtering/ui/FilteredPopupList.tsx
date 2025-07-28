e3b084768eed3d96050010579a2a16ccb0e4df37
Jade <92345556+jjeongsu@users.noreply.github.com>
2025-07-28 18:56:44 +0900

    fix : 빌드오류(잘못된 import 경로) 해결 (#126)

--- frontend/src/features/filtering/ui/FilteredPopupList.tsx
+++ frontend/src/features/filtering/ui/FilteredPopupList.tsx
@@ -1,11 +1,15 @@
 import popupList from '@/entities/popup/api/data';
-import PopupListView from '@/entities/popup/ui/PopupListView';
+import BadgedPopupCard from '@/entities/popup/ui/BadgedPopupCard';
 
 export default function FilteredPopupList() {
   return (
     <div className={'flex flex-col gap-y-2 px-5 pt-4.5  bg-white'}>
       <h2 className={'font-semibold text-xl text-black '}>찾은 팝업</h2>
-      <PopupListView data={popupList} />
+      <div className={'flex flex-col gap-y-3 mt-4 pb-[90px]'}>
+        {popupList.map((popup, index) => (
+          <BadgedPopupCard {...popup} key={index} />
+        ))}
+      </div>
     </div>
   );
 }
--- frontend/src/shared/ui/badge/Badge.tsx
+++ frontend/src/shared/ui/badge/Badge.tsx
@@ -3,7 +3,7 @@ import { Slot } from '@radix-ui/react-slot';
 import { cva, type VariantProps } from 'class-variance-authority';
 
 import { cn } from '@/lib/utils';
-import IconClock from '@/assets/icons/Normal/Icon_Clock.svg?react';
+import IconClock from '@/assets/icons/Normal/Icon_Clock.svg';
 
 export const badgeVariants = cva(
   'inline-flex items-center justify-center gap-1 px-2 py-1 text-xs font-medium w-fit whitespace-nowrap shrink-0 text-white rounded-md leading-none',
