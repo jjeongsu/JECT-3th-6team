d47fe6b4a664178a547b65a10e4d68c2d58ccd10
임수빈 <robin980108@naver.com>
2025-08-11 11:13:02 +0900

    fix: 타임존 GMT+09 로 수정 (#164)

--- backend/src/main/java/com/example/demo/Demo2Application.java
+++ backend/src/main/java/com/example/demo/Demo2Application.java
@@ -5,12 +5,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
 public class Demo2Application {
 
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("GMT+09:00"));
         SpringApplication.run(Demo2Application.class, args);
     }
 }
